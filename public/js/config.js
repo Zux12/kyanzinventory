df6574f384328435ae42d67379778da5881269a2
Zux12 <158550944+Zux12@users.noreply.github.com>
2026-01-21 09:10:39 +0800

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+// public/js/config.js
+// If running on Heroku, leave empty and it will use same-origin.
+// If frontend hosted elsewhere, set: window.API_BASE = "https://your-app.herokuapp.com";
+window.API_BASE = "";
